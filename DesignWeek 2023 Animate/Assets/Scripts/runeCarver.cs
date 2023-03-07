40ef7ab00e8e844319bfaf651d3a844f05a00090
BubblesTheDev <78316409+BubblesTheDev@users.noreply.github.com>
2023-03-07 16:30:14 -0500

    Prototype Carving works

--- DesignWeek 2023 Animate/Assets/Scripts/runeCarver.cs
+++ DesignWeek 2023 Animate/Assets/Scripts/runeCarver.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 
-public class runeCarver : MonoBehaviour
-{
+public class runeCarver : MonoBehaviour {
     [Header("Graphics")]
     [SerializeField] private Image runeCircle;
     [SerializeField] private GameObject runeBook;
@@ -20,12 +19,28 @@ public class runeCarver : MonoBehaviour
     [SerializeField] private float errorDistance;
 
     private void Update() {
-        
+        if (Input.GetKey(KeyCode.Mouse0) || Input.GetKey(KeyCode.Mouse1)) {
+            carveRune();
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0) || Input.GetKeyUp(KeyCode.Mouse1)) {
+            removeLine();
+        }
     }
 
     void removeLine() {
         carvingLine.positionCount = 0;
     }
 
+    void carveRune() {
 
+        if (carvingLine.positionCount == 0) {
+            carvingLine.positionCount++;
+            carvingLine.SetPosition(0, Camera.main.ScreenToViewportPoint(Input.mousePosition + new Vector3(0, 0, Camera.main.nearClipPlane)));
+
+        } else if (Vector3.Distance(Camera.main.ScreenToViewportPoint(Input.mousePosition + new Vector3(0, 0, Camera.main.nearClipPlane)), carvingLine.GetPosition(carvingLine.positionCount - 1)) >= maxDistanceBetweenPoints) {
+            carvingLine.positionCount++;
+            carvingLine.SetPosition(carvingLine.positionCount - 1, Camera.main.ScreenToViewportPoint(Input.mousePosition + new Vector3(0, 0, Camera.main.nearClipPlane)));
+        } else if (carvingLine.positionCount > 50) removeLine();
+        
+    }
 }
