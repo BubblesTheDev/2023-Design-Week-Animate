aeda5084e457da3d5f0968cf46b052a54a67cfcb
BubblesTheDev <78316409+BubblesTheDev@users.noreply.github.com>
2023-03-07 14:29:42 -0500

    Changed Scene Stuff Added Scripts

---
+++ DesignWeek 2023 Animate/Assets/Scripts/runeDataContainer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class runeDataContainer : MonoBehaviour
+{
+    
+}
+
+public enum runeTypes {
+    personality,
+    motivation,
+    job
+}
+
+public enum personalityTypes {
+
+}
+
+public enum motivationLevels{
+
+}
+
+public enum jobs {
+
+}
