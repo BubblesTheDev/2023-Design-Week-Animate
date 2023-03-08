bbfc589aa7d62bdefeda85209ce42cf106b14d8e
BubblesTheDev <78316409+BubblesTheDev@users.noreply.github.com>
2023-03-08 01:23:22 -0500

    Added editor scripting to make creating runes easier

--- DesignWeek 2023 Animate/Assets/Scripts/runeDataContainer.cs
+++ DesignWeek 2023 Animate/Assets/Scripts/runeDataContainer.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEditor;
 [CreateAssetMenu(menuName = "Create New Rune", fileName = "New Rune")]
 public class runeDataContainer : ScriptableObject
 {
-    [SerializeField] private runeTypes runeType;
+    public runeTypes runeType;
+    public string runeDescription;
 
-    [SerializeField] private personalityTypes personality;
-    [SerializeField] private motivationLevels motivation;
-    [SerializeField] private jobs runeJob;
-    [SerializeField] private Sprite runeAsset;
-    [SerializeField] private List<GameObject> linePlaces;
+    public float manaCost;
+    public personalityTypes personality;
+    public motivationLevels motivation;
+    public jobs runeJob;
+    public Sprite runeAsset;
+    public List<GameObject> linePlaces;
 }
 
 public enum runeTypes {
@@ -38,4 +40,38 @@ public enum jobs {
     None,
     Builder,
     Breaker
+}
+
+[CustomEditor(typeof(runeDataContainer))]
+public class runeDataEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        runeDataContainer container = (runeDataContainer)target;
+
+        SerializedProperty m_runeAssetSprite = serializedObject.FindProperty("runeAsset");
+        SerializedProperty m_linePlaces = serializedObject.FindProperty("linePlaces");
+
+        container.runeType = (runeTypes)EditorGUILayout.EnumPopup("Rune Type: ", container.runeType);
+        EditorGUILayout.LabelField("Rune Description: ");
+        container.runeDescription = EditorGUILayout.TextArea("Write description for rune here", GUILayout.Height(50));
+
+        EditorGUILayout.Space(20);
+        switch (container.runeType)
+        {
+            case runeTypes.job:
+                container.runeJob = (jobs)EditorGUILayout.EnumPopup("Item's Job: ", container.runeJob);
+                break;
+            case runeTypes.motivation:
+                container.motivation = (motivationLevels)EditorGUILayout.EnumPopup("Motivation Level: ", container.motivation);
+                break;
+            case runeTypes.personality:
+                container.personality = (personalityTypes)EditorGUILayout.EnumPopup("Object's Personality: ", container.personality);
+                break;
+        }
+        container.manaCost = EditorGUILayout.FloatField("Rune Mana Cost: ", container.manaCost);
+        EditorGUILayout.PropertyField(m_runeAssetSprite,new GUIContent("Rune Asset Sprite: "));
+        EditorGUILayout.PropertyField(m_linePlaces, new GUIContent("Rune line Points: "));
+        serializedObject.ApplyModifiedProperties();
+    }
 }
