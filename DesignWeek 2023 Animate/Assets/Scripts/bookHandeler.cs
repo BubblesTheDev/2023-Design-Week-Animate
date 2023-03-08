9a6ffb46f777daeba2efb4f807cc2c57f1ba0908
BubblesTheDev <78316409+BubblesTheDev@users.noreply.github.com>
2023-03-08 14:51:25 -0500

    Pushing For saving

--- DesignWeek 2023 Animate/Assets/Scripts/bookHandeler.cs
+++ DesignWeek 2023 Animate/Assets/Scripts/bookHandeler.cs
@@ -15,19 +15,23 @@ public class bookHandeler : MonoBehaviour
     [Space, Header("Graphic Data")]
     [SerializeField] private GameObject bookObject;
     [SerializeField] private GameObject runeSelectorPrefab;
+    [SerializeField] private List<GameObject> pages;
 
     private void Awake()
     {
+
         createRunesForPages();
     }
 
     private void Update()
     {
-        if (carverScript.objToCarve != null && !bookObject.activeSelf) bookObject.SetActive(true);
 
-        if (Input.GetKeyDown(KeyCode.F1)) currentRunePageIndex = 0;
-        if (Input.GetKeyDown(KeyCode.F2)) currentRunePageIndex = 1;
-        if (Input.GetKeyDown(KeyCode.F3)) currentRunePageIndex = 2;
+        if (Input.GetKeyDown(KeyCode.F1)) setPage(0);
+        if (Input.GetKeyDown(KeyCode.F2)) setPage(1);
+        if (Input.GetKeyDown(KeyCode.F3)) setPage(2);
+
+
+        handlePageGraphics();
     }
 
     void createRunesForPages()
@@ -36,12 +40,16 @@ public class bookHandeler : MonoBehaviour
         {
             for (int x = 0; x < runePageData[i].runes.Count; x++)
             {
-                GameObject temp = Instantiate(runeSelectorPrefab, GameObject.Find("Content").transform.position, Quaternion.identity, GameObject.Find("Content").transform);
+                GameObject temp = Instantiate(runeSelectorPrefab, GameObject.Find("Content " + runePageData[i].name).transform.position, Quaternion.identity, GameObject.Find("Content " + runePageData[i].name).transform);
                 temp.name = "Rune Button " + runePageData[i].runes[x].name;
+                temp.transform.Find("Rune Name").gameObject.GetComponent<Text>().text = "Name: " + runePageData[i].runes[x].name;
+                temp.transform.Find("Rune Description").gameObject.GetComponent<Text>().text = "Description: " + runePageData[i].runes[x].runeDescription;
                 temp.transform.Find("RunePicture").gameObject.GetComponent<Image>().sprite = runePageData[i].runes[x].runeAsset;
                 temp.GetComponent<Button>().onClick.AddListener( delegate { GameObject.Find("Game Manager").GetComponent<bookHandeler>().selectRune(); });
             }
         }
+        bookObject.transform.Find("LeftPage").gameObject.SetActive(false);
+        bookObject.transform.Find("RightPage").gameObject.SetActive(false);
     }
 
     public void selectRune()
@@ -51,5 +59,58 @@ public class bookHandeler : MonoBehaviour
             carverScript.runeSelected = runePageData[currentRunePageIndex].runes.Where(runeDataContainer => runeDataContainer.name == new string(thingCalling.name.Replace("Rune Button ", ""))).FirstOrDefault();
             carverScript.runeTraceImage.sprite = carverScript.runeSelected.runeAsset;
         }
+    }
+
+    public void handlePageGraphics() {
+        switch (currentRunePageIndex) {
+            case 0:
+            pages[0].SetActive(true);
+            pages[1].SetActive(false);
+            pages[2].SetActive(false);
+            break;
+            case 1:
+            pages[1].SetActive(true);
+            pages[2].SetActive(false);
+            pages[0].SetActive(false);
+            break;
+            case 2:
+            pages[2].SetActive(true);
+            pages[0].SetActive(false);
+            pages[1].SetActive(false);
+            break;
+        }
+    }
+
+    public void setPage(int pageIndex) {
+        if (pageIndex + 1 > pages.Count) print("There is no page at the index: " + (pageIndex+1));
+        else currentRunePageIndex = pageIndex;
+    }
+
+    public void enableBookWrapper() {
+        StartCoroutine(enableBook());
+    }
+
+    public void disableBookWrapper() {
+        StartCoroutine(disableBook());
+    }
+
+    public IEnumerator enableBook() {
+        bookObject.SetActive(true);
+        while(bookObject.transform.position != Vector3.zero) {
+           bookObject.transform.Translate((Vector3.zero - bookObject.transform.position) * Time.deltaTime * 25f);
+            bookObject.transform.localScale = Vector3.Lerp(Vector3.one * .25f, Vector3.one * 2, Time.deltaTime * 5f);
+            yield return new WaitForEndOfFrame();
+        }
+
+        //Once book is in center of screen, play animation of opening
+        //wait for animation to finish
+        yield return new WaitForSeconds(5f);
+
+        bookObject.transform.Find("Right Page").gameObject.SetActive(true);
+        bookObject.transform.Find("Left Page").gameObject.SetActive(true);
+    }
+
+    public IEnumerator disableBook() {
+        yield return new WaitForEndOfFrame();
     }
 }
